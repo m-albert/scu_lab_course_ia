eaf25a454ec0fd1fd9927bc52e2cca410a2122b8
Marvin Albert <marvin.albert@gmail.com>
2025-09-17 17:36:11 +0200

    Add cellpose test

--- notebooks/tests/test_imports.py
+++ notebooks/tests/test_imports.py
@@ -5,3 +5,18 @@ def test_imports():
     import skimage
     from cellpose import models
     
+
+def test_cellpose():
+    """
+    Test that
+    - cellpose can be imported
+    - a model can be instantiated
+    - the model can be run on a random image
+    """
+    from cellpose import models
+    import numpy as np
+
+    model = models.Cellpose(gpu=False, model_type='cyto')
+    img = np.random.rand(100, 100)
+    masks, flows, styles, diams = model.eval(img, diameter=None, channels=[0, 0])
+    assert masks.shape == img.shape
