741b19e0fe74ad946c5563a29883fa3edf3e8ddd
Marvin Albert <marvin.albert@gmail.com>
2025-09-22 21:38:11 +0200

    Add pyimagej test

--- notebooks/tests/test_imports.py
+++ notebooks/tests/test_imports.py
@@ -20,3 +20,40 @@ def test_cellpose():
     img = np.random.rand(100, 100)
     masks, flows, styles, diams = model.eval(img, diameter=None, channels=[0, 0])
     assert masks.shape == img.shape
+
+
+def test_pyimagej():
+    import os
+    import imagej
+    import scyjava
+    import imageio
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from skimage.measure import label
+    from skimage.io import imsave, imread
+    from iaf.plot import imshow, show_labels
+    from iaf.morph.watershed import estimate_object_sizes, separate_neighboring_objects
+
+    # Initialize ImageJ with Fiji (can take minutes to initialize) 
+    ij = imagej.init('sc.fiji:fiji')
+
+    img = imread('illustrations/blobs.gif')
+
+    img = ij.io().open('illustrations/blobs.gif')
+
+    # Convert to ImagePlus (Java class) for compatibility with WekaSegmentation
+    imp = ij.py.to_imageplus(img)
+
+    # Create WekaSegmentation object with the image
+    WekaSegmentation = scyjava.jimport('trainableSegmentation.WekaSegmentation')
+    weka = WekaSegmentation(imp)
+
+    # Load pre-trained classifier model (change to your .model file path)
+    classifier_path = r'illustrations/classifier.model'
+    weka.loadClassifier(classifier_path)
+
+    # Apply the classifier to image, arguments: (ImagePlus, threads=0 auto, getProbabilities=False)
+    result_imp = weka.applyClassifier(imp, 0, False)
+
+    img_py = ij.py.from_java(result_imp)
+    img_py_n = img_py.to_numpy()
